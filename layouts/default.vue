842a59393036ed5c1542f0f082e7cefe02883037
kevineyon1 <kevingideon123@gmail.com>
2021-12-22 12:55:32 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    <app-sidebar></app-sidebar>
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <app-header></app-header>
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/layouts/Sidebar.vue";
+import Header from "@/components/layouts/Header.vue";
+
+export default {
+  components: {
+    "app-sidebar": Sidebar,
+    "app-header": Header,
+  },
+}
+</script>
